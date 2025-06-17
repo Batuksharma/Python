2f13e4d8aa31de574162be9776653ff2bdcdac2c
Batuk Sharma <batuksharmaofficial@gmail.com>
2025-06-17 13:43:50 +0530

    Chapter 1 -2

---
+++ Chapter1/Problem1.py
@@ -0,0 +1,14 @@
+print('''To pull the metal splinter from my palm
+my father recited a story in a low voice.
+I watched his lovely face and not the blade.
+Before the story ended, he’d removed
+the iron sliver I thought I’d die from.
+
+I can’t remember the tale,
+but hear his voice still, a well
+of dark water, a prayer.
+And I recall his hands,
+two measures of tenderness
+he laid against my face,
+the flames of discipline
+he raised above my head.''')
---
+++ Chapter1/Problem2.py
@@ -0,0 +1,4 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("Get out and move out")
+engine.runAndWait()
