7de78954495c568b8a1ab4958b49bed0393bf1ac
Batuk Sharma <batuksharmaofficial@gmail.com>
2025-06-23 13:14:41 +0530

    Commit 4

--- Chapter3/practice.py
+++ Chapter3/practice.py
@@ -1,3 +1,4 @@
+#Problem1
 name = input("Enter your name: ")
 print(f"Good Morning,{name}")
 
@@ -5,4 +6,17 @@ letter = '''Dear name,
             You are selected.
             Date'''
 
-print(letter.replace("name","Batuk").replace("Date","24/6/2025"))
+print(letter.replace("name","Batuk").replace("Date","24/6/2025")) # strings are immutable, this function does not change the original string, it prints the new string
+
+
+
+#Problem2
+text = "Alertcode is a great company"
+print(text.find("c")) # this returns index if found, e;se it returns -1
+
+
+
+#Problem3
+sentence = "Dear Batuk,\nThis python course is amazing,\nThanks"
+print(sentence)
+
