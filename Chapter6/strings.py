cf39480655c5ed5d08708483bd4746bf46b3b2b3
Batuksharma <batuksharmaofficial@gmail.com>
2026-02-09 21:45:41 +0530

    Chapter 6

---
+++ Chapter6/strings.py
@@ -0,0 +1,90 @@
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+a = "Hello"
+print(a)
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+#Strings are Arrays
+a = "Hello, World!"
+print(a[1]) #e
+
+
+#Looping Through a String
+for x in "banana":
+  print(x)
+
+
+#String Length
+a = "Hello, World!"
+print(len(a))
+
+
+#Check String
+txt = "The best things in life are free!"
+print("free" in txt) 
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
+
+
+#slicing strings
+b = "Hello, World!"
+print(b[2:5])
+
+b = "Hello, World!"
+print(b[:5])
+
+b = "Hello, World!"
+print(b[2:])
+
+b = "Hello, World!"
+print(b[-5:-2])
+
+
+
+#Modify Strings
+a = "Hello, World!"
+print(a.upper())
+
+a = "Hello, World!"
+print(a.lower())
+
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
+
+
+
+#f string and placeholders
+age = 23
+txt = f"My name is John and my age is {age}"
+print(txt)
+
+price = 62
+txt = f"The price of chocolate is {price:.2f}"
+print(txt)
+
+txt = f"The price is {20 * 59} dollars"
+print(txt)
+
+
