d870c2420f629bafd3c94d26ed63a4db5b1fe4b1
Batuksharma <batuksharmaofficial@gmail.com>
2026-02-09 21:50:45 +0530

    Commit 17

--- Chapter6/strings.py
+++ Chapter6/strings.py
@@ -87,4 +87,15 @@ print(txt)
 txt = f"The price is {20 * 59} dollars"
 print(txt)
 
+#escape characters
+txt = "We are the so-called \"Vikings\" from the north."
 
+# \'	Single Quote	
+# \\	Backslash	
+# \n	New Line	
+# \r	Carriage Return	
+# \t	Tab	
+# \b	Backspace	
+# \f	Form Feed	
+# \ooo	Octal value	
+# \xhh	Hex value
